a9fa7be04c39dc1dd6f8272badf1db4eef36a67e
KuroAshi75 <95868857+KuroAshi75@users.noreply.github.com>
2021-12-09 20:38:29 +0530

    Add files via upload

---
+++ 3-moduleExports.js
@@ -0,0 +1,9 @@
+const names = require('./4-moduleNames')
+const sayHi = require('./5-moduleUtils')
+const singlePerson = require('./6-moduleExportsArray')
+ require('./7-moduleFunction')
+
+console.log(singlePerson)
+
+sayHi('pavan')
+sayHi(names.john) 
---
+++ 4-moduleNames.js
@@ -0,0 +1,9 @@
+// local
+const john = 'john'
+
+// share
+const peter = 'peter'
+
+module.exports = { john }
+
+console.log(module)
